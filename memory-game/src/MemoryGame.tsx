b61a8c3299f62e8f7fa1f8c94e5245feb203bb34
Alex Geis <siegxela@gmail.com>
2023-01-13 10:15:49 -0700

    remove App.tsx boilerplate content and create starting files for MemoryGame and css module

---
+++ memory-game/src/MemoryGame.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./MemoryGame.module.css";
+
+function MemoryGame() {
+	return (
+		<div className={styles.wrapper}>
+			<div></div>
+		</div>
+	);
+}
+
+export default MemoryGame;
