cdfbda884c1bb7896235ca2f5d03e8a851e5a4c1
Alex Geis <siegxela@gmail.com>
2023-01-13 10:06:46 -0700

    add footer boilerplate, add relevant assets for footer images, and import footer into App.tsx

---
+++ memory-game/src/Footer.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import githubLogo from "./assets/icons/footer/github-mark.svg";
+import linkedInLogo from "./assets/icons/footer/linkedin-round-icon.svg";
+import spotifyLogo from "./assets/icons/footer/spotify-icon.svg";
+import styles from "./Footer.module.css";
+
+function Footer() {
+	return (
+		<div className={styles.wrapper}>
+			<a
+				href="https://github.com/alexgeis"
+				target="_blank"
+			>
+				<div className={styles.iconWrapper}>
+					<img
+						src={githubLogo}
+						alt="github"
+					/>
+				</div>
+			</a>
+			<a
+				href="https://www.linkedin.com/in/alexngeis/"
+				target="_blank"
+			>
+				<div className={styles.iconWrapper}>
+					<img
+						src={linkedInLogo}
+						alt="linkedIn"
+					/>
+				</div>
+			</a>
+			<a
+				href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA"
+				target="_blank"
+			>
+				<div className={styles.iconWrapper}>
+					<img
+						src={spotifyLogo}
+						alt="spotify"
+					/>
+				</div>
+			</a>
+		</div>
+	);
+}
+
+export default Footer;
