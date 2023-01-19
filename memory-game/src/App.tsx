3f2d1416f3141dd73f2d697189fbda87778865fd
Alex Geis <siegxela@gmail.com>
2023-01-19 11:02:26 -0700

    style memory cards and add hover/active styling

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Header from "./Header";
+import MemoryGame from "./MemoryGame";
 import Footer from "./Footer";
 import "./App.css";
 
@@ -8,6 +9,8 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
+			<MemoryGame />
+			<div className="spacer"></div>
 			<Footer />
 		</div>
 	);
