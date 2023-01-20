2c50b6b5b8255d3a018e55de70a7cd8f69a560a1
Alex Geis <siegxela@gmail.com>
2023-01-20 14:21:08 -0700

    add test starting code to app.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,25 @@
 // Imports
-// import { render, screen } from "@testing-library/react";
-// import user from "@testing-library/user-event";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 // To Test
 import App from "../App";
 
 // Tests
+describe("App component", () => {
+	it("renders new user score as 0", () => {
+		// since screen does not have the container property, we'll destructure render to obtain container for this test
+		const { container } = render(<App />);
+		expect(container).toMatchSnapshot();
+	});
+
+	it("increase score by 1 after card click", () => {
+		render(<App />);
+		const img = screen.getByRole("img", { name: "" });
+
+		userEvent.click(img);
+
+		expect(screen.getByRole("heading").textContent).toMatch("1");
+	});
+});
