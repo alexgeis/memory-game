22ded4c2802f01f4879a9fb483f85ef0509b4a50
Alex Geis <siegxela@gmail.com>
2023-01-20 10:24:54 -0700

    fix git commit history

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,10 @@ import { render } from "@testing-library/react";
 import { describe, expect, test } from "@jest/globals";
 import App from "../App";
 
+test("that jest is working", () => {
+	expect(true).toBe(true);
+});
+
 describe("App component", () => {
 	it("renders correct heading", () => {
 		const { getByRole } = render(<App />);
